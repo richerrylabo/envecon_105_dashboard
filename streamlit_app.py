d04532adf58cc798ca6967385764097fae2406c7
richerrylabo <richerry@berkeley.edu>
2025-08-16 02:03:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.title("My First Dashboard Test")
+st.set_page_config(layout="wide")
+st.title("Dashboard Demo")
 
-# Load your wrangled CSV
-df = pd.read_csv("data/wrangled_data_env.csv")
+# Load both datasets
+df_group = pd.read_csv("data/wrangled_data_group.csv")
+df_individual = pd.read_csv("data/wrangled_data_individual.csv")
 
-st.subheader("Preview of dataset")
-st.write(df.head())
+st.subheader("Group Dataset (first 5 rows)")
+st.dataframe(df_group.head(), use_container_width=True)
+
+st.subheader("Individual Dataset (first 5 rows)")
+st.dataframe(df_individual.head(), use_container_width=True)
 
